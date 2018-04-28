65500d212a8e7cb7d8fbe7ffb1946e307ad361e0
cucaracha <ya.tarakanov.ilya@yandex.ru>
2018-04-28 03:36:59 +0300

    fix execquery max item id

--- src/UserManager_SynUserAskDBCallback.cpp
+++ src/UserManager_SynUserAskDBCallback.cpp
@@ -55,7 +55,7 @@ namespace MS {
             pDBConnector->ExecQuery("select max(id) from (select unnest(comp_id) as id from game_user) as foo");
             pDBConnector->GetQueryFieldData("max", m_n32MaxComputerID);
             pDBConnector->CloseQuery();
-            pDBConnector->ExecQuery("select max(id) from (select unnest(item_id) as id from computer_user) as foo");
+            pDBConnector->ExecQuery("select max(id) from (select id from item_user) as foo");
             pDBConnector->GetQueryFieldData("max", m_n32MaxItemDBID);
             pDBConnector->CloseQuery();
             LogPrint(LogFlags::ALL, "MaxComputerID: %d\n", m_n32MaxComputerID);
