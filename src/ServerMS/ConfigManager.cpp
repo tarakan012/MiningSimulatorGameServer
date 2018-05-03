44856d334ed3dd5549775033d59d30023b9d5ae2
cucaracha <ya.tarakanov.ilya@yandex.ru>
2018-05-03 11:20:43 +0300

    update config manager

--- src/ServerMS/ConfigManager.cpp
+++ src/ServerMS/ConfigManager.cpp
@@ -1,7 +1,8 @@
 #include "ServerMS/ConfigManager.h"
-#include "Json.h"
-#include "ServerMS/ConfigConst.h"
+
 #include <fstream>
+#include "fast-cpp-csv-parser/csv.h"
+#include "nlohmann/json.hpp"
 
 using namespace Log;
 
@@ -18,134 +19,82 @@ namespace ServerMS {
     bool CConfigManager::LoadShopCfg() {
         m_ShopCfgMap.clear();
         std::ifstream file;
+        nlohmann::json json;
         file.open("../ThirdFunc/ConfigFile/ShopCfg.json");
         if (!file.is_open()) {
             return false;
         }
-        file.seekg(0, file.end);
-        INT32 length = file.tellg();
-        file.seekg(0, file.beg);
-        CHAR *pBuffer = new CHAR[length];
-        file.read(pBuffer, length);
-
-        Json *json = Json_create(pBuffer);
-        delete[]pBuffer;
-        Json *obj = json->child;
-        for (INT32 i = 0; i < json->size; ++i) {
+        json << file;
+        for (auto element : json) {
+            nlohmann::json page_shop(element);
+            INT32 PageNumber = page_shop.at("PageNumber").get<INT32>();
+            INT32 Type = page_shop.at("Type").get<INT32>();
+            nlohmann::json ItemIDArray = page_shop.at("ItemIDArray");
             std::map<INT32, SItemRecord> PageShopMap;
-            INT32 n32PageType = Json_getInt(obj, "typePage", -1);
-
-            Json *jsonArray = Json_getItem(obj, "itemIdPage");
-
-            Json *JsonNumber = jsonArray->child;
-            for (INT32 i = 0; jsonArray->size; ++i) {
-                INT32 n32ItemId = JsonNumber->valueInt;
-                auto iter = m_ItemRecordMap.find(n32ItemId);
+            for (auto item_id : ItemIDArray) {
+                auto iter = m_ItemRecordMap.find(item_id);
                 if (iter != m_ItemRecordMap.end()) {
-                    PageShopMap[n32ItemId] = iter->second;
+                    PageShopMap[item_id] = iter->second;
                 } else {
-                    LogPrint(LogFlags::CFG, "The wrong ItemID - %d\n", n32ItemId);
+                    LogPrint(LogFlags::CFG, "The wrong ItemID - %d\n", item_id);
                     return false;
                 }
-                if (!JsonNumber->next) {
-                    break;
-                }
-                JsonNumber = JsonNumber->next;
             }
-            m_ShopCfgMap[n32PageType] = PageShopMap;
-            if (!obj->next) {
-                break;
-            }
-            obj = obj->next;
+            m_ShopCfgMap[Type] = PageShopMap;
         }
-
-        Json_dispose(json);
-        file.close();
         return true;
-
     }
 
     bool CConfigManager::LoadItemCfg() {
         m_ItemRecordMap.clear();
-        std::ifstream file;
-        file.open("../ThirdFunc/ConfigFile/ItemCfg.json");
-        if (!file.is_open()) {
-            return false;
-        }
-        file.seekg(0, file.end);
-        INT32 length = file.tellg();
-        file.seekg(0, file.beg);
-        CHAR *pBuffer = new CHAR[length];
-        file.read(pBuffer, length);
 
-        Json *json = Json_create(pBuffer);
-        delete[]pBuffer;
-        Json *obj = json->child;
-        for (INT32 i = 0; i < json->size; ++i) {
-            SItemRecord item;
-            const CHAR *nameItem = Json_getString(obj, "name", "");
-            std::memcpy(&item.szName, nameItem, std::strlen(nameItem));
-            item.n32Id = Json_getInt(obj, "id", -1);
-            item.n32Type = Json_getInt(obj, "type", -1);
-            item.n32Lvl = Json_getInt(obj, "lvl", -1);
-            item.n32Icon = Json_getInt(obj, "icon", -1);
-            item.n32MiningGold = Json_getInt(obj, "miningGold", -1);
-            item.n32MiningResourses = Json_getInt(obj, "miningResourses", -1);
-            item.n32PowerConsumption = Json_getInt(obj, "powerConsumption", -1);
-            item.n32ExpansionSlot = Json_getInt(obj, "expansionSlot", -1);
-            item.n32LvlGrind = Json_getInt(obj, "lvlGrind", -1);
-            item.n32Cost = Json_getInt(obj, "cost", -1);
-            item.n32Strength = Json_getInt(obj, "strength", -1);
+        io::CSVReader<11> reader("../ThirdFunc/ConfigFile/ItemConfig.csv");
+
+        reader.read_header(io::ignore_extra_column, "id", "name", "type", "lvl", "icon_id", "mining_gold",
+                           "mining_resources", "energy_consumption", "strength", "cost", "number_slots");
+        SItemRecord item;
+
+        while (reader.read_row(item.n32Id,
+                               item.NameStr,
+                               item.n32Type,
+                               item.n32Lvl,
+                               item.n32Icon,
+                               item.n32MiningGold,
+                               item.n32MiningResourses,
+                               item.n32EnergyConsumption,
+                               item.n32Strength,
+                               item.n32Cost,
+                               item.n32NumberSlots)) {
             m_ItemRecordMap[item.n32Id] = item;
-
-            if (!obj->next) {
-                break;
-            }
-            obj = obj->next;
         }
 
-        Json_dispose(json);
-        file.close();
         return true;
     }
 
     bool CConfigManager::LoadStartSetCfg() {
         std::ifstream file;
+        nlohmann::json json;
         file.open("../ThirdFunc/ConfigFile/StartSetCfg.json");
         if (!file.is_open()) {
             return false;
         }
         m_sStartSetCfg.sPODUserDBData.n32Energy = nMAX_ENERGY;
-        file.seekg(0, file.end);
-        INT32 length = file.tellg();
-        file.seekg(0, file.beg);
-        CHAR *pBuffer = new CHAR[length];
-        file.read(pBuffer, length);
-
-        Json *json = Json_create(pBuffer);
-        delete[]pBuffer;
-        Json *obj = json->child;
-
-        for (INT32 numcomp = 0; numcomp < json->size; ++numcomp) {
-            Json *jsonArray = Json_getItem(obj, "list_id");
-            Json *JsonNumber = jsonArray->child;
-            for (INT32 numitem = 0; numitem < jsonArray->size; ++numitem) {
-                INT32 n32ItemID = JsonNumber->valueInt;
-                auto iter = m_ItemRecordMap.find(n32ItemID);
+        json << file;
+        for (auto element : json) {
+            nlohmann::json computer(element);
+            INT32 CompID = computer.at("CompID").get<INT32>();
+            nlohmann::json ItemIDArray = computer.at("ItemIDArray");
+            std::map<INT32, SItemRecord> PageShopMap;
+            for (auto item_id : ItemIDArray) {
+                auto iter = m_ItemRecordMap.find(item_id);
                 if (iter != m_ItemRecordMap.end()) {
-                    m_sStartSetCfg.CompInfoMap[numcomp].ItemRecordMap[numitem] = iter->second;
+                    m_sStartSetCfg.CompInfoMap[CompID].ItemRecordMap[item_id] = iter->second;
                 } else {
-                    LogPrint(LogFlags::CFG, "The wrong ItemID - %d\n", n32ItemID);
+                    LogPrint(LogFlags::CFG, "The wrong ItemID - %d\n", item_id);
                     return false;
                 }
-                if (!JsonNumber->next) {
-                    break;
-                }
-                JsonNumber = JsonNumber->next;
             }
         }
-        Json_dispose(json);
-        file.close();
         return true;
     }
 
